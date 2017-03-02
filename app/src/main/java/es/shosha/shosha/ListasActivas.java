ff14a863792df30e7bfb4c4ef6aaa4549412dc49
Inés Hernández Casado <ines.hernandez@alum.unirioja.es>
2017-03-02 18:01:47 +0100

    Split 1 - Formato listas activas y botón astrás

--- app/src/main/java/es/shosha/shosha/ListasActivas.java
+++ app/src/main/java/es/shosha/shosha/ListasActivas.java
@@ -1,18 +1,26 @@
 package es.shosha.shosha;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class ListasActivas extends Activity {
+public class ListasActivas extends AppCompatActivity {
+
     private ListView list;
     private String[] listas={"Navidad", "Casa", "Cena 28/02/2017"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listas_activas);
         list=(ListView)findViewById(R.id.listasActivas);
         ArrayAdapter<String> adaptador=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listas);
@@ -23,7 +31,21 @@ public class ListasActivas extends Activity {
                 //Acción a ejecutar al seleccionar elemento de la lista
             }
         });
-        super.onCreate(savedInstanceState);
+        //Aparece el botón de atrás
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
